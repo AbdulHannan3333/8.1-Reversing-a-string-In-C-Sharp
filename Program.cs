459819da566c142ab2b7c40c5c6fc1ba95337877
AbdulHannan3333 <hannan67212@outlook.com>
2024-06-16 02:22:18 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1_Reversing_a_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello World!";
+            char[] charArray = input.ToCharArray();
+            Console.WriteLine(charArray.Length);
+            Array.Reverse(charArray);
+            string reseverd = new string(charArray);
+            Console.WriteLine($"Resevered String: {reseverd}");    
+        }
+    }
+}
